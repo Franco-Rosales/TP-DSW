d6095ea67dcfdf324f4b87febc57ce02fc8370e9
Franco-Rosales <rosalesfranco17@gmail.com>
2024-06-28 17:06:54 -0300

    correccion de archivos

--- app/db/init.js
+++
@@ -1,6 +0,0 @@
-const crearDatos = async () => {
-    console.log('Inicialización sin datos ejecutada.');
-    // Aquí no hacemos nada, solo dejamos la función como placeholder
-};
-
-module.exports = crearDatos;
