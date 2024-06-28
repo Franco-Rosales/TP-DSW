ec71385d5edca759e4c3bf913d226b072e90f752
Franco-Rosales <rosalesfranco17@gmail.com>
2024-06-28 17:33:24 -0300

    actualizacion de entorno de desarrollo

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,11 @@
-
+import React from 'react';
 
 function App() {
   return (
     <>
+      <div className='container text-center'>
+        <h1 className='mt-5'>Welcome to the React App</h1>
+      </div>
     </>
   );
 }
