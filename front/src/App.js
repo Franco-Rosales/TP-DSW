37fbefc4aa7003c0ab43e85d7bfb94837cd6010a
frosales <frosales@vates.com>
2024-07-02 13:11:46 -0300

    Actualizando nav y app

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route} from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { Recetas } from './components/Recetas/Recetas';
-import { ListarReceta } from './components/Recetas/Listado'
 import { Chefs } from './components/Chefs/Chefs';
 import { EditarChef } from './components/Chefs/EditarChef';
 import { EditarReceta } from './components/Recetas/EditarReceta';
 import { Comentarios } from './components/Comentarios/Comentarios';
 import { EditarComentario } from './components/Comentarios/EditarComentario';
 import { Categorias } from './components/Categorias/Categorias';
+import { Contacto } from './components/Contacto/Contacto';
 
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
         <Route path="/comentarios" element={<Comentarios/>}/>
         <Route path="/comentarios/:id" element={<EditarComentario/>}/>
         <Route path="/categorias" element={<Categorias/>}/>
+        <Route path='/contacto' element={<Contacto/>} />
       </Routes>
   );
 }
--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -24,6 +24,9 @@ export const NavBar = () => {
         <Nav.Item>
           <Nav.Link as={Link} to="/categorias" className='nav-link text-light'>Categorias</Nav.Link>
         </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/contacto" className='nav-link text-light'>Contacto</Nav.Link>
+        </Nav.Item>
       </Nav>
     </>
   );
